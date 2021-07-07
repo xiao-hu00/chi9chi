d2ce45f34837bf337fdb490314a71e24132528ad
wanli <wanli.hu@evatarchina.com>
2021-07-07 14:22:48 +0800

    init

---
+++ _nuxt/d31ec7c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{198:function(n,e,t){"use strict";t.r(e);var l=t(41),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  吃玖吃 螺蛳粉\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
